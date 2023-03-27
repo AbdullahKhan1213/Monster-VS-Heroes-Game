156ee2e4f95ad224aedf43c7d89d96b88d43f15d
AbdullahKhan1213 <128769404+AbdullahKhan1213@users.noreply.github.com>
2023-03-27 16:09:49 -0600

    Update Menu.java

--- MonstersvsHeros/src/mvh/Menu.java
+++ MonstersvsHeros/src/mvh/Menu.java
@@ -7,8 +7,7 @@ import java.util.Scanner;
 
 /**
  * Simple menu so user can enter commands to continue or stop simulation
- * @author Jonathan Hudson
- * @version 1.0
+ 
  */
 public final class Menu {
 
